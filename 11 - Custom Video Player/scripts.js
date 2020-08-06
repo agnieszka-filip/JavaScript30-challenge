8fc90c1f92c5cd5f53912644675ff5d72f0f5e26
agnieszka-filip <agnieszka.fi94@gmail.com>
2020-08-06 21:58:02 +0200

    Day 11: Add functionalities for skip buttons and two ranges at the bottom

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -15,7 +15,18 @@ function updateButton() {
   toggle.textContent = this.paused ? "►" : "❚ ❚";
 }
 
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function updateRange() {
+  video[this.name] = this.value;
+}
+
 video.addEventListener("click", togglePlay);
 toggle.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+skipButtons.forEach((skipButton) => skipButton.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", updateRange));
+ranges.forEach((range) => range.addEventListener("mousemove", updateRange));

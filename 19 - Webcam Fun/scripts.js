350bcf10f2a49c7dd7d6be85233b4e677248cc31
agnieszka-filip <agnieszka.fi94@gmail.com>
2020-08-19 16:46:26 +0200

    Day 19: Add color split and green screen effects

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -16,8 +16,6 @@ const getVideo = () => {
     });
 };
 
-getVideo();
-
 const showOnCanvas = () => {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -26,6 +24,16 @@ const showOnCanvas = () => {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    //pixels = addRedFilter(pixels);
+
+    //pixels = splitColors(pixels);
+    //ctx.globalAlpha = 0.1;
+
+    pixels = addGreenScreen(pixels);
+
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 };
 
@@ -40,5 +48,52 @@ const takePhoto = () => {
   link.innerHTML = `<img src="${data}" alt="A girl boss" />`;
   strip.insertBefore(link, strip.firstChild);
 };
+
+const addRedFilter = (pixels) => {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 60;
+    pixels.data[i + 1] = pixels.data[i + 1] - 70;
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.6;
+  }
+  return pixels;
+};
+
+const splitColors = (pixels) => {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 500] = pixels.data[i + 1];
+    pixels.data[i - 550] = pixels.data[i + 2];
+  }
+  return pixels;
+};
+
+const addGreenScreen = (pixels) => {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+  return pixels;
+};
+
+getVideo();
 
 video.addEventListener("canplay", showOnCanvas);
